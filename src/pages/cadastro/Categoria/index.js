081d3c727bb8c53e684f0f28c294edb1dee93a4b
marcospsw <marcospsw96@gmail.com>
2020-07-31 17:29:39 -0300

    Atualização Host

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,11 +29,11 @@ function CadastroCategoria() {
 
   useEffect(() => {
     // eslint-disable-next-line no-console
-    const URL = 'http://localhost:8080/categorias';
+    const URL = 
     
-      // window.location.hostname.includes('localhost')
-      // ? 'http://localhost:8080/categorias'
-      // : 'https:/fourflix.herokuapp.com/categorias';
+      window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'http:/fourflix.herokuapp.com/categorias';
 
     fetch(URL)
       .then(async (respostaDoServidor) => {
